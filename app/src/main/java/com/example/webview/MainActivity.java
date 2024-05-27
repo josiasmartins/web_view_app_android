6c6b629d8f9b54dcbd2bfacac6db18da8d1b3c33
Caitano <josias.m.caitano@accenture.com>
2024-05-27 16:22:40 -0300

    feature: initial project

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.webview;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Chronometer;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private WebView webView;
+
+    private String urlToLoad = "https://josiasmartins.com"
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_main);
+
+       // assignviewbyid
+        progressBar = findViewById(R.id.loading_progressbar);
+        webView = findViewById(R.id.webview);
+
+        webView.loadUrl(urlToLoad);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                getSupportActionBar().setTitle(title);
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+
+        }
+    }
+
+}
