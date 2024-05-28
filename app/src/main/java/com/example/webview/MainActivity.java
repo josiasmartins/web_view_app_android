046b81944ea1615239ee750b1ea3c0b8fb49eac2
Caitano <josias.m.caitano@accenture.com>
2024-05-28 12:52:35 -0300

    fix: fixed web view android in website josiasmartins.com/

--- app/src/main/java/com/example/webview/MainActivity.java
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.webview;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Chronometer;
 import android.widget.ProgressBar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -16,49 +24,66 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ProgressBar progressBar;
     private WebView webView;
-
-    private String urlToLoad = "https://josiasmartins.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-       // assignviewbyid
-        progressBar = findViewById(R.id.loading_progressbar);
-        webView = findViewById(R.id.webview);
+        final ProgressBar progressBar = findViewById(R.id.progress);
+        progressBar.setVisibility(View.INVISIBLE);
 
-        webView.loadUrl(urlToLoad);
-
-        webView.setWebViewClient(new WebViewClient());
+        WebView webView = findViewById(R.id.webview);
+        webView.loadUrl("https://josiasmartins.com/");
         webView.getSettings().setJavaScriptEnabled(true);
 
-        webView.setWebChromeClient(new WebChromeClient() {
+        webView.setWebViewClient(new WebViewClient(){
             @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                super.onProgressChanged(view, newProgress);
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE); // mostra a progress
             }
 
             @Override
-            public void onReceivedTitle(WebView view, String title) {
-                super.onReceivedTitle(view, title);
-                getSupportActionBar().setTitle(title);
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.INVISIBLE); // esconde a progress
             }
-
         });
 
     }
 
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
+//    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+//    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+//        if (request.getUrl().getHost().equals("tiagoaguiar.co")) {
+//            return false;
+//        }
+//
+//        Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+//        startActivity(intent);
+//        return true;
+//    }
 
+
+//    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//        if (Uri.parse(url).getHost().equals("tiagoaguiar.co")) {
+//            return false;
+//        }
+//
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//        startActivity(intent);
+//        return true;
+//    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();
+            return true;
         }
+
+        return super.onKeyDown(keyCode, event);
     }
 
 }
